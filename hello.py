a2c67ce243e92edc3697b9b83382f7e4580836ea
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-01-27 15:46:38 -0600

    chanhe to hello2

--- hello.py
+++ hello.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def source():
-    for i in range(100):
-        print(i)
-    
-    html = 'Hello World!'
-    return html
+def main_python():
+    return "Hello 2!"
+
+# @app.route('/')
+# def source():
+#     for i in range(100):
+#         print(i)
+#
+#     html = 'Hello World!'
+#     return html
