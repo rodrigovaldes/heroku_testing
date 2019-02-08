5a4d864ddf533c07bf7fb11e107ee210e4e78b4b
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-07 20:43:46 -0600

    changes 3

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn hello:app
 clock: python clock.py
--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def main_python():
-    control = 0
-    for i in range(100):
-        control += i
-    str_control = str(control)
-
-    return str_control
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def main_python():
+#     control = 0
+#     for i in range(100):
+#         control += i
+#     str_control = str(control)
+#
+#     return str_control
 
 
 # @app.route('/')
