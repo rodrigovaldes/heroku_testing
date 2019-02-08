ea740ca3ad936e7e1c140cf4cb64a91f0dc4cfc7
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-07 20:40:09 -0600

    add requirements3

--- clock.py
+++ clock.py
@@ -5,6 +5,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every three minutes.')
+    print(datetime.datetime.now())
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
