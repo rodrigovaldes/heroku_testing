4162957a2f5335622282c21a095c9e3ec158a8db
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-10 13:53:49 -0600

    try all days 2pm

--- clock.py
+++ clock.py
@@ -3,13 +3,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every three minutes.')
-    print(datetime.datetime.now())
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     print('This job is run every three minutes.')
+#     print(datetime.datetime.now())
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14)
+def scheduled_job():
+    print('This job is run every weekday at 2pm.')
 
 sched.start()
