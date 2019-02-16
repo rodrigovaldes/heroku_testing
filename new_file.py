c2c68ee971f68daca34be2ecef62ae295de773d6
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-16 17:49:38 -0600

    nb4 master

--- new_file.py
+++ new_file.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 
 print("I am testing the branches. Like in this case")
-n='hello'
+n='i can not believe you did not say hi'
 
 # I believe that one is not the best number. We should do something different
