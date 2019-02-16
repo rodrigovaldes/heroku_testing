fbc09231f3848ab60ed448f212560ede991f15f5
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-16 17:30:25 -0600

    pr4

--- new_file.py
+++ new_file.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 
 print("I am testing the branches. Like in this case")
-n=8482402
-m=3
-w=4
-
+n='hello'
 
 # I believe that one is not the best number. We should do something different
